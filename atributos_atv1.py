66cf6c6e057bbb696f71ab0794064c82b8649514
henriquefirbidagirotto-cmd <henriquefirbidagirotto@gmail.com>
2026-04-23 21:48:25 -0300

    Add files via upload

--- atributos_atv1.py
+++ atributos_atv1.py
@@ -147,13 +147,12 @@ class Aluno:
     def __init__(self, nome, matricula):
         self.nome = nome
         self.matricula = matricula
-        self.__notas = []  # lista privada
-
+        self.__notas = []
     def adicionar_nota(self, nota):
-        if 0 <= nota <= 10:
+        if nota >= 0 and nota <= 10:
             self.__notas.append(nota)
         else:
-            print("Nota inválida! Digite entre 0 e 10.")
+            print("Nota invalida! Digite entre 0 e 10.")
 
     def calcular_media(self):
         if len(self.__notas) == 0:
@@ -171,7 +170,7 @@ class Aluno:
             return "Reprovado"
 
     def __str__(self):
-        return f"Aluno: {self.nome} | Média: {self.calcular_media():.2f} | Situação: {self.situacao()}"
+        return f'Aluno: {self.nome} Média: {self.calcular_media():.2f} Situação: {self.situacao()}'
 
 aluno1 = Aluno('Mateus', 1222)
 
@@ -189,18 +188,83 @@ print(aluno1)
 # Crie uma classe `Data` com atributos `dia`, `mes`, `ano`. No `__init__`, valide se a data é válida (considere meses com 30/31 dias e ano bissexto para fevereiro).
 #  Use propriedades para garantir que alterações futuras também sejam validadas. Adicione um método `__str__` que retorna a data no formato `dd/mm/aaaa`.
 
+class Data:
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+
+    def verificar(self):
+        meses   =  [0,1,2,3,4,5,6,7,8,9,10,11,12]
+        dias_meses = [0,list(range(0,31)),
+                    list(range(0,29)),
+                    list(range(0,31)),
+                    list(range(0,30)),
+                    list(range(0,31)),
+                    list(range(0,30)),
+                    list(range(0,31)),
+                    list(range(0,31)),
+                    list(range(0,30)),
+                    list(range(0,31)),
+                    list(range(0,30)),
+                    list(range(0,31))
+                    ]
+
+
+
+        if (self.ano % 400 == 0 or self.ano % 4 == 0) or (self.ano % 100 ==0):
+            print('ano bissexto...')
+        else:
+            print('Não é bissexto...')
+
+        if self.mes in meses:
+            i =  meses.index(self.mes)
+            if self.dia in dias_meses[i]:
+                print('data válida')
+        else:
+            print('Data invalida')
+
+
+
+    def __str__(self):
+        return self.dia, self.mes, self.ano     
+
+
+d = Data(1,11,1988)
+d.verificar()
+
 # ---
 
 # ### **Exercício 7 – Funcionário com Aumento**
 
 # Classe `Funcionario` com atributos: `nome`, `cargo`, `salario_base` (privado). Métodos:
-
 # - `aumentar_salario(percentual)` – aumenta o salário.
 # - `calcular_bonus()` – retorna 10% do salário base.
 # - Propriedade `salario` para leitura.
     
 #     Teste criando um funcionário, aumente o salário e mostre o novo valor.
     
+class Funcionario:
+    def __init__(self, nome, cargo, salario):
+        self._nome = nome
+        self._cargo =  cargo
+        self._salario = salario
+
+
+    def aumentar_salario(self, percentual):
+        n_sal =  self._salario * (percentual /100) 
+        return n_sal
+    
+    def calcular_bonus(self):
+        x  =  self.aumentar_salario(10)
+        self._salario += self._salario * 0.10
+        v_total =  self._salario + x
+        return v_total
+    
+
+p = Funcionario('kaio', 'dev', 5000)
+print(p.aumentar_salario(10))
+print(p.calcular_bonus())
 
 # ---
 
@@ -215,6 +279,30 @@ print(aluno1)
 #     Teste acelerando e freando.
     
 
+class Carro:
+    def __init__(self, marca, modelo):
+        self.marca =  marca
+        self.modelo =  modelo
+        self.__velocidade = 0
+
+    def aumenta_velocidade(self, valor):
+          for x in range(valor):
+              self.__velocidade += valor
+              return   self.__velocidade
+          
+   
+    def frear(self, valor):
+        
+        for x in range(self.__velocidade, valor,-1):
+            self.__velocidade -= valor
+            return self.__velocidade
+
+
+c =  Carro('jeep', '2026')
+print(c.aumenta_velocidade(151))
+print(c.frear(50))
+
+
 # ---
 
 # ### **Exercício 9 – Estatísticas (Atributos de Classe)**
@@ -226,6 +314,25 @@ print(aluno1)
     
 #     Use `@classmethod` e não crie instâncias. Teste adicionando números e exibindo a média.
     
+class Estatistica: 
+       soma  =  0
+       contagem  =  0
+
+       @classmethod
+       def adcionar(cls, valor):
+              cls.soma += valor
+              cls.contagem += 1
+
+       @classmethod
+       def calcular_media(cls):
+              if cls.contagem ==  0:
+                     return 0
+              return cls.soma / cls.contagem
+
+Estatistica.adcionar(10)
+Estatistica.adcionar(5)
+Estatistica.adcionar(7)
+print(Estatistica.calcular_media())        
 
 # ---
 
@@ -238,3 +345,51 @@ print(aluno1)
 # - `buscar_contato(nome)` – exibe os dados do primeiro contato com aquele nome.
     
 #     Teste adicionando vários contatos e fazendo buscas.
+
+class Contatos:
+    def __init__(self, nome, telefone, email):
+        self.nome  = nome
+        self.telefone = telefone
+        self.email  = email
+        
+
+
+
+class Agenda:
+    def __init__(self):
+        self.__lista = []
+
+    def add(self, contato):
+        self.__lista.append(contato)
+      
+
+    def listar_contatos(self):
+        return self.__lista
+        
+        
+
+    def buscar(self,nome) :
+        if nome in self.__lista:
+            return nome
+        else:
+            return 'não existe...'   
+
+
+c =  Contatos('a','132','a@gmail.com')
+c2 =  Contatos('b','132','b@gmail.com')
+c3 =  Contatos('b','132','b@gmail.com')
+agenda = Agenda()
+
+
+
+# agenda.add(c)
+# agenda.add(c2)
+# agenda.add(c3)
+agenda.add(c.nome)
+agenda.add(c.telefone)
+agenda.add(c.email)
+agenda.add(c2.nome)
+agenda.add(c2.telefone)
+agenda.add(c2.email)
+print(agenda.listar_contatos())
+print(agenda.buscar('a'))
